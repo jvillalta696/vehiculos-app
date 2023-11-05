e8facbfdd13ac68ad5812ccb35d91a332b5301e2
jvillalta696 <jvillalta696@gmail.com>
2023-11-05 10:09:19 -0600

    modify sigIn method and add signout method

--- src/libs/auth.js
+++ src/libs/auth.js
@@ -1,5 +1,5 @@
 import { auth } from "../backend/firebase.js";
-import {  createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {  createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 export const signUp = async(email,password)=>{
     createUserWithEmailAndPassword(auth, email, password)
@@ -15,20 +15,22 @@ export const signUp = async(email,password)=>{
   });
 };
 
-export const signIn = async(email,password)=>{
-    signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+export const signIn = async(email,password)=>{    
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    console.log(userCredential.user)
+    return userCredential
+  } catch (error) {
+   throw error
+  }
 }
 
-export const createUser = async()=>{
-    
+export const signout = async()=>{
+
+  signOut(auth).then(() => {
+  console.log('logout successfully')
+}).catch((error) => {
+  throw error
+});
 }
 
