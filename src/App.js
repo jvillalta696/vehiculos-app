14516d2e578d5a6b8cdd5c783acfc61fff5a8d0a
jvillalta696 <jvillalta696@gmail.com>
2023-11-06 18:58:14 -0600

    create app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import LoginForm from './components/forms/LoginForm';
+import { AuthProvider } from './contexts/AuthContext';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import ProtectedRoute from './libs/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm/>
-    </div>
+    <AuthProvider>
+    <Routes>
+    <Route  path="/login" element={<Login/>  }/>  
+    <Route path='/' element={
+      <ProtectedRoute>
+         <Dashboard/>
+      </ProtectedRoute>     
+    }/>     
+  </Routes>
+  </AuthProvider>
   );
 }
 
