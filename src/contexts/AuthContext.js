2825b25e626e9a3425c68bd663bdbe44537f7da1
jvillalta696 <jvillalta696@gmail.com>
2023-11-06 18:57:31 -0600

    create context for user data

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,68 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
+    setPersistence,
+    browserSessionPersistence,
+
+} from "firebase/auth";
+import { auth } from '../backend/firebase';
+import { getById } from "../services/firestore.service";
+
+const authContext = createContext();
+
+export const useAuth = () => {
+    const context = useContext(authContext);
+    return context;
+}
+
+export const AuthProvider = ({ children }) => {
+
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+    const [config, setConfig]=useState(null)
+
+    const signIn = async(email, psw) => {
+        try {
+            setPersistence(auth, browserSessionPersistence)
+                .then(() => {
+                    return signInWithEmailAndPassword(auth, email, psw);
+                })
+            return signInWithEmailAndPassword(auth, email, psw)           
+            .catch((e)=>{throw e})
+        } catch (error) {
+            throw error
+        }
+
+    };
+    const signout = () => signOut(auth)
+
+    const getConfig = async()=>{
+        if(user && !config){
+            const usrConf = await getById('Usuarios',user.uid);
+            setConfig(usrConf.data());
+            console.log(usrConf.data());
+        }
+    }
+
+    useEffect(() => {
+        const unsubscirbe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);            
+            setLoading(false);            
+            console.log('authContext_uf')
+        })
+        return () => { unsubscirbe() }
+    }, []);
+
+    useEffect(() => {
+       getConfig();
+    }, [user]);
+
+    return (
+        <authContext.Provider value={{ signIn, signout, user, loading, config }}>
+            {children}
+        </authContext.Provider>
+    );
+
+}
